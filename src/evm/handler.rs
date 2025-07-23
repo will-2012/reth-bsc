a07abe4339f0e9f24bad4951445dc3a5900a78da
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-07-23 15:56:55 +0800

    chore: update reward_beneficiary

--- src/evm/handler.rs
+++ src/evm/handler.rs
@@ -67,15 +67,15 @@ impl<DB: Database, INSP> Handler for BscHandler<DB, INSP> {
 
         let effective_gas_price = ctx.effective_gas_price();
         let gas = exec_result.gas();
-        let mut tx_fee = U256::from(gas.spent() - gas.refunded() as u64) * effective_gas_price;
+        let tx_fee = U256::from(gas.spent() - gas.refunded() as u64) * effective_gas_price;
 
         // EIP-4844
-        let is_cancun = SpecId::from(ctx.cfg().spec()).is_enabled_in(SpecId::CANCUN);
-        if is_cancun {
-            let data_fee = tx.calc_max_data_fee();
-            tx_fee = tx_fee.saturating_add(data_fee);
-        }
-
+        // let is_cancun = SpecId::from(ctx.cfg().spec()).is_enabled_in(SpecId::CANCUN);
+        // if is_cancun {
+        //     let data_fee = tx.calc_max_data_fee();
+        //     tx_fee = tx_fee.saturating_add(data_fee);
+        // }
+        println!("tx_caller: {:?}, tx_fee: {:?}", tx.caller(), tx_fee);
         let system_account = ctx.journal_mut().load_account(SYSTEM_ADDRESS)?;
         system_account.data.mark_touch();
         system_account.data.info.balance = system_account.data.info.balance.saturating_add(tx_fee);
