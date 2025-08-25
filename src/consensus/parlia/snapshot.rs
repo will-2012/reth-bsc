190b0d0ba244d26c7263736fd8ebffcd748cf510
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-08-25 14:28:44 +0800

    chore: reduce logs

--- src/consensus/parlia/snapshot.rs
+++ src/consensus/parlia/snapshot.rs
@@ -312,7 +312,7 @@ impl Snapshot {
         for (&block, &v) in &self.recent_proposers {
             if block <= left_bound || v == Address::default() { continue; }
             *counts.entry(v).or_insert(0) += 1;
-            tracing::debug!("count_recent_proposers, block: {:?}, validator: {:?}, count: {:?}", block, v, counts.get(&v).unwrap());
+            // tracing::debug!("count_recent_proposers, block: {:?}, validator: {:?}, count: {:?}", block, v, counts.get(&v).unwrap());
         }
         counts
     }
