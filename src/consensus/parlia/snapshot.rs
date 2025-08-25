b136f1543660562b0fc30d5fefe61440760a8d16
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-08-25 11:00:59 +0800

    chore: polish some codes

--- src/consensus/parlia/snapshot.rs
+++ src/consensus/parlia/snapshot.rs
@@ -325,12 +325,10 @@ impl Snapshot {
     /// Helper that takes pre-computed counts.
     pub fn sign_recently_by_counts(&self, validator: Address, counts: &HashMap<Address, u8>) -> bool {
         if let Some(&times) = counts.get(&validator) {
-            let allowed = u64::from(self.turn_length.unwrap_or(1)); // has bug?? 
+            let allowed = u64::from(self.turn_length.unwrap_or(1));
             if u64::from(times) >= allowed { 
                 tracing::warn!("Recently signed, validator: {:?}, block_number: {:?}, times: {:?}, allowed: {:?}", validator, self.block_number, times, allowed);
-                // TODO: fix it later.
                 return false;
-                //return true; 
             }
         }
         false
