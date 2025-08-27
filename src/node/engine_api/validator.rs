1864da6a3d9d58c65314fce88398d5941c117664
2020xibao <117156346+will-2012@users.noreply.github.com>
2025-08-27 09:29:17 +0800

    chore: remove useless code

--- src/node/engine_api/validator.rs
+++ src/node/engine_api/validator.rs
@@ -1,10 +1,8 @@
 use super::payload::BscPayloadTypes;
-// use crate::consensus::parlia::seal::{default_sign_fn, SealBlock};
-// use crate::shared::get_snapshot_provider;
 use crate::{chainspec::BscChainSpec, hardforks::BscHardforks, BscBlock, BscPrimitives};
 use alloy_consensus::BlockHeader;
 use alloy_eips::eip4895::Withdrawal;
-use alloy_primitives::{Address, B256};
+use alloy_primitives::B256;
 use alloy_rpc_types_engine::PayloadError;
 use reth::{
     api::{FullNodeComponents, NodeTypes},
@@ -14,7 +12,6 @@ use reth::{
     },
     consensus::ConsensusError,
 };
-use reth_chainspec::EthChainSpec;
 use reth_engine_primitives::{ExecutionPayload, PayloadValidator};
 use reth_payload_primitives::NewPayloadError;
 use reth_primitives::{RecoveredBlock, SealedBlock};
@@ -128,24 +125,8 @@ where
     ) -> Result<SealedBlock<BscBlock>, PayloadError> {
         let block = payload.0;
 
-        // let snapshot_provider = if let Some(provider) = get_snapshot_provider() {
-        //     provider.clone()
-        // } else {
-        //     tracing::error!("Failed to register Parlia RPC due to can not get snapshot provider");
-        //     return Err(NewPayloadError::Other(Box::from("Failed to get snapshot provider")))
-        // };
-        //
-        // let validator_address = Address::default(); // TODO get validator_address from config
-        // let seal_block_gen = SealBlock::new_with_sign_fn(
-        //     snapshot_provider,
-        //     self.inner.clone(),
-        //     validator_address,
-        //     default_sign_fn,
-        // );
-
         let expected_hash = block.header.hash_slow();
         let sealed_block = block.seal_slow();
-        // let sealed_block = seal_block_gen.seal(block).map_err(NewPayloadError::other)?;
 
         // Ensure the hash included in the payload matches the block hash
         if expected_hash != sealed_block.hash() {
